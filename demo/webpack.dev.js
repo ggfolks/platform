ba9295a178e3cefaa8b108ecdd46010aacfec2b8
Michael Bayne <mdb@samskivert.com>
2019-07-08 16:43:22 -0700

    Webpack twiddling to get source maps to mostly work.
    Emphasis on mostly, as is par for the course with everything JavaScript.

--- demo/webpack.dev.js
+++ demo/webpack.dev.js
@@ -1,14 +1,23 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.core.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: './dist',
     compress: true,
     disableHostCheck: true,
     host: '0.0.0.0',
     port: 3000
-  }
+  },
+  plugins: [
+    // apply this plugin only to .ts files - the rest is taken care of
+    new webpack.SourceMapDevToolPlugin({
+      filename: null,
+      exclude: [/node_modules/],
+      test: /\.ts($|\?)/i
+    })
+  ]
 })
