5dbfb73bd99fb6d01339c9cce889a6c0536a99bd
Michael Bayne <mdb@samskivert.com>
2019-07-29 13:11:39 -0700

    Text selection.
    Single, double & triple clicks do the right thing, and (1/2/3) click and drag.
    Also cut and paste on browsers that support it (yay).

--- src/ui/text.ts
+++ src/ui/text.ts
@@ -5,7 +5,8 @@ import {Mutable, Subject, Value} from "../core/react"
 import {makeRectPath} from "./util"
 import {Control, ControlConfig, ControlStates, Element, ElementConfig, ElementContext,
         MouseInteraction} from "./element"
-import {Spec, FontConfig, PaintConfig, DefaultPaint, ShadowConfig, Span, EmptySpan} from "./style"
+import {Spec, FontConfig, Paint, PaintConfig, DefaultPaint, ShadowConfig,
+        Span, EmptySpan} from "./style"
 
 const tmpr = rect.create()
 
@@ -15,6 +16,9 @@ export interface LabelStyle {
   fill? :Spec<PaintConfig>
   stroke? :Spec<PaintConfig>
   shadow? :Spec<ShadowConfig>
+  selection? :{
+    fill? :Spec<PaintConfig>
+  }
 }
 
 /** Defines configuration for [[Label]]. */
@@ -27,11 +31,16 @@ export interface LabelConfig extends ElementConfig {
 /** Displays styled text. */
 export class Label extends Element {
   readonly xoffset = Mutable.local(0)
+  readonly selection = Mutable.local<[number,number]>([0,0])
   readonly span = this.observe(EmptySpan)
+  readonly selFill = this.observe<Paint|undefined>(undefined)
   readonly text :Value<string>
+  private selOff = 0
+  private selWid = 0
 
   constructor (ctx :ElementContext, parent :Element, readonly config :LabelConfig) {
     super(ctx, parent, config)
+    this.invalidateOnChange(this.selection)
     this.text = ctx.resolveModel(config.text)
     this.state.onValue(state => {
       const style = this.getStyle(this.config.style, state)
@@ -42,6 +51,8 @@ export class Label extends Element {
         const shadow = ctx.resolveShadowOpt(style.shadow)
         return new Span(text, font, fill, stroke, shadow)
       }))
+      if (!style.selection || !style.selection.fill) this.selFill.update(undefined)
+      else this.selFill.observe(ctx.resolvePaint(style.selection.fill))
     })
   }
 
@@ -51,8 +62,14 @@ export class Label extends Element {
 
   protected relayout () {
     // clamp our x offset so we don't have empty space on the right (TODO: or left?)
-    const width = this.width, swidth = this.span.current.size[0]
+    const width = this.width, span = this.span.current, swidth = span.size[0]
     if (this.xoffset.current + swidth < width) this.xoffset.update(width-swidth)
+    // if we have a selection, compute its x position & width
+    const [ss, se] = this.selection.current
+    if (ss < se) {
+      this.selOff = span.measureAdvance(ss)
+      this.selWid = span.measureAdvance(se) - this.selOff
+    } else this.selWid = 0
   }
 
   protected rerender (canvas :CanvasRenderingContext2D) {
@@ -64,38 +81,107 @@ export class Label extends Element {
       makeRectPath(canvas, x, y, width, height)
       canvas.clip()
     }
+    // if we have a selection fill and a selection, render it
+    const selFill = this.selFill.current, selOff = this.selOff, selWid = this.selWid
+    if (selFill && selWid > 0) {
+      selFill.prepFill(canvas)
+      canvas.fillRect(rx + selOff, y, selWid, height)
+    }
+    // render our label
     span.render(canvas, rx, y)
     if (needClip) canvas.restore()
   }
 }
 
-type TextAction = (text :Mutable<string>, cursor :Mutable<number>, typed :string) => void
+async function readClipText () :Promise<string|undefined> {
+  if (navigator.clipboard.readText) return navigator.clipboard.readText()
+  // note: Firefox doesn't support clipboard-read permission yet... sigh
+  const desc = {name: "clipboard-read"} as any as PermissionDescriptor
+  const res = await navigator.permissions.query(desc)
+  if (res.state === "granted" || res.state === "prompt") return navigator.clipboard.readText()
+  else return undefined
+}
+
+type TextState = {
+  text: Mutable<string>,
+  cursor: Mutable<number>,
+  selection: Mutable<[number,number]>
+}
+type TextAction = (state :TextState, typed :string) => void
+
+// TODO: do we want to support a simple undo mechanism for text editing? would be nice if it
+// factored into a larger undo framework...
 
 const actions :PMap<TextAction> = {
   // text edits
-  insert: (tx, cs, typed) => {
-    const ctext = tx.current, ipos = cs.current
-    tx.update(ctext.substring(0, ipos) + typed + ctext.substring(ipos))
-    cs.update(ipos+1)
+  insert: (state, typed) => {
+    const ctext = state.text.current, [sstart, send] = state.selection.current
+    if (sstart < send) {
+      state.text.update(ctext.substring(0, sstart) + typed + ctext.substring(send))
+      state.cursor.update(sstart+1)
+      state.selection.update([0, 0])
+    } else {
+      const ipos = state.cursor.current
+      state.text.update(ctext.substring(0, ipos) + typed + ctext.substring(ipos))
+      state.cursor.update(ipos+1)
+    }
   },
-  delete: (tx, cs, typed) => {
-    const ctext = tx.current, ipos = cs.current
+  delete: (state, typed) => {
+    const ctext = state.text.current, ipos = state.cursor.current
     if (ipos < ctext.length) {
-      tx.update(ctext.substring(0, ipos) + ctext.substring(ipos+1))
+      state.text.update(ctext.substring(0, ipos) + ctext.substring(ipos+1))
     }
   },
-  backspace: (tx, cs, typed) => {
-    const ctext = tx.current, ipos = cs.current
+  backspace: (state, typed) => {
+    const ctext = state.text.current, ipos = state.cursor.current
     if (ipos > 0) {
-      tx.update(ctext.substring(0, ipos-1) + ctext.substring(ipos))
-      cs.update(ipos-1)
+      state.text.update(ctext.substring(0, ipos-1) + ctext.substring(ipos))
+      state.cursor.update(ipos-1)
     }
+  },
+  // clipboard interaction
+  cut: (state, typed) => {
+    const [sstart, send] = state.selection.current
+    if (send > sstart) {
+      const ctext = state.text.current
+      state.text.update(ctext.substring(0, sstart) + ctext.substring(send))
+      state.selection.update([0,0])
+      state.cursor.update(sstart)
+      navigator.clipboard.writeText(ctext.substring(sstart, send))
+    }
+  },
+  copy: (state, typed) => {
+    const [sstart, send] = state.selection.current
+    if (send > sstart) navigator.clipboard.writeText(state.text.current.substring(sstart, send))
+  },
+  paste: (state, typed) => {
+    const ctext = state.text.current, cpos = state.cursor.current
+    const [sstart, send] = state.selection.current
+    const [pos, pre, post] = (send > sstart) ?
+      [sstart, ctext.substring(0, sstart), ctext.substring(send)] :
+      [cpos, ctext.substring(0, cpos), ctext.substring(cpos)]
+    readClipText().then(text => {
+      if (text) {
+        const trimmed = text.replace("\n", "").replace("\r", "")
+        state.text.update(pre + trimmed + post)
+        state.cursor.update(pos + trimmed.length)
+        state.selection.update([0, 0])
+      }
+    })
   },
   // moving the cursor around
-  cursorStart: (tx, cs, typed) => cs.update(0),
-  cursorLeft: (tx, cs, typed) => cs.update(Math.max(cs.current-1, 0)),
-  cursorRight: (tx, cs, typed) => cs.update(Math.min(cs.current+1, tx.current.length)),
-  cursorEnd: (tx, cs, typed) => cs.update(tx.current.length),
+  cursorStart: (state, typed) => {
+    state.cursor.update(0)
+  },
+  cursorLeft: (state, typed) => {
+    state.cursor.update(Math.max(state.cursor.current-1, 0))
+  },
+  cursorRight: (state, typed) => {
+    state.cursor.update(Math.min(state.cursor.current+1, state.text.current.length))
+  },
+  cursorEnd: (state, typed) => {
+    state.cursor.update(state.text.current.length)
+  },
 }
 
 const ShiftMask = 1 << 0
@@ -119,10 +205,17 @@ const keyMap :KeyMap = {
   // "Standard" key bindings
   Backspace: {0: "backspace"},
   Delete: {0: "delete"},
-  ArrowLeft: {0: "cursorLeft", [ShiftMask]: "cursorStart"},
-  ArrowRight: {0: "cursorRight", [ShiftMask]: "cursorEnd"},
+  // TODO: Shift-Arrow should extend selection left & right
+  ArrowLeft: {0: "cursorLeft", [CtrlMask]: "cursorStart"},
+  ArrowRight: {0: "cursorRight", [CtrlMask]: "cursorEnd"},
   Home: {0: "cursorStart"},
   End: {0: "cursorEnd"},
+
+  // TODO: make keymap dynamically, passing "standard" modifier mask so we can
+  // create macOS keymaps that use Meta & Linux/Windows keymaps that use Ctrl
+  KeyX: {[CtrlMask]: "cut", [MetaMask]: "cut"},
+  KeyC: {[CtrlMask]: "copy", [MetaMask]: "copy"},
+  KeyV: {[CtrlMask]: "paste", [MetaMask]: "paste"},
 
   // Emacs key bindings
   KeyA: {[CtrlMask]: "cursorStart"},
@@ -173,6 +266,54 @@ export class Cursor extends Element {
   }
 }
 
+const wordBRegex = new RegExp("\\b")
+
+function wordBounds (words :string[], pos :number) :[number, number] {
+  let off = 0
+  for (let ww = 0; ww < words.length; ww += 1) {
+    const wend = off + words[ww].length
+    if (wend > pos) return [off, wend]
+    off = wend
+  }
+  // if we fell off the end, say we're in the final word
+  return [off-words[words.length-1].length, off]
+}
+
+type Selector = (moff :number, state :TextState) => void
+
+function charSelector (text :string, doff :number, state :TextState) :Selector {
+  state.selection.update([doff, doff])
+  state.cursor.update(doff)
+  return (moff, state) => {
+    state.cursor.update(moff)
+    const mino = Math.min(doff, moff), maxo = Math.max(doff, moff)
+    state.selection.update([mino, maxo])
+  }
+}
+
+function wordSelector (text :string, doff :number, state :TextState) :Selector {
+  const words = text.split(wordBRegex)
+  const dbounds = wordBounds(words, doff)
+  state.selection.update(dbounds)
+  // put the cursor at the end of the selection
+  state.cursor.update(dbounds[1])
+  return (moff, state) => {
+    const mbounds = wordBounds(words, moff)
+    if (mbounds[0] < dbounds[0]) {
+      state.selection.update([mbounds[0], dbounds[1]])
+      state.cursor.update(mbounds[0])
+    } else {
+      state.selection.update([dbounds[0], mbounds[1]])
+      state.cursor.update(mbounds[1])
+    }
+  }
+}
+
+function allSelector (text :string, doff :number, state :TextState) :Selector {
+  state.selection.update([0, text.length])
+  return (moff, state) => state.cursor.update(moff)
+}
+
 const DefaultCursor :CursorConfig = {type: "cursor", style: {}}
 
 /** Defines configuration for [[Text]]. */
@@ -185,48 +326,69 @@ export interface TextConfig extends ControlConfig {
 /** Displays a span of editable text. */
 export class Text extends Control {
   private readonly jiggle = Mutable.local(false)
+  private readonly textState :TextState
   readonly coffset = Mutable.local(0)
   readonly text :Mutable<string>
-  readonly cursor :Cursor
   readonly label :Label
+  readonly cursor :Cursor
 
   constructor (ctx :ElementContext, parent :Element, readonly config :TextConfig) {
     super(ctx, parent, config)
     this.invalidateOnChange(this.coffset)
     this.text = ctx.resolveModel(config.text)
 
-    // we include jiggle here so that we can reset the clock fold on demand when the user clicks the
-    // mouse even when we're already focused
-    const blinking = Value.join2(this.state, this.jiggle).switchMap(([state, jiggle]) => {
-      if (state !== "focused") return Value.constant(false)
-      else {
-        const blinkPeriod = this.cursor.config.blinkPeriod || DefaultBlinkPeriod
-        return this.root.clock.fold(0, (acc, c) => acc+c.dt, refEquals).
-          map(acc => Math.floor(acc/blinkPeriod) % 2 === 0)
-      }
-    })
-    const cconfig = {...(config.cursor || DefaultCursor), visible: blinking}
-    this.cursor = ctx.createElement(this, cconfig) as Cursor
-
     const label = this.contents.findChild("label")
     if (label) this.label = label as Label
     else throw new Error(`Text control must have Label child [config=${JSON.stringify(config)}].`)
+
+    // hide the cursor when the label has a selection
+    const hasSel = this.label.selection.map(([ss, se]) => se > ss)
+    // we include jiggle here so that we can reset the clock fold on demand when the user clicks the
+    // mouse even when we're already focused
+    const blinking = Value.join3(this.state, hasSel, this.jiggle).switchMap(
+      ([state, hasSel, jiggle]) => {
+        if (state !== "focused" || hasSel) return Value.constant(false)
+        else {
+          const blinkPeriod = this.cursor.config.blinkPeriod || DefaultBlinkPeriod
+          return this.root.clock.fold(0, (acc, c) => acc+c.dt, refEquals).
+            map(acc => Math.floor(acc/blinkPeriod) % 2 === 0)
+        }
+      })
+    const cconfig = {...(config.cursor || DefaultCursor), visible: blinking}
+    this.cursor = ctx.createElement(this, cconfig) as Cursor
+
+    this.textState = {text: this.text, cursor: this.coffset, selection: this.label.selection}
   }
 
   get styleScope () { return {id: "text", states: ControlStates} }
 
   handleMouseDown (event :MouseEvent, pos :vec2) :MouseInteraction|undefined {
     if (event.button !== 0) return undefined
+
     // if we're already focused, jiggle the cursor blinker so it restarts; this ensures that the
     // user immediately sees the cursor at the new location
     if (this.isFocused) this.jiggle.update(!this.jiggle.current)
     else this.focus()
-    // position the cursor based on where the click landed
-    const cp = pos[0] - this.label.x - this.label.xoffset.current
-    this.coffset.update(this.label.span.current.computeOffset(cp))
-    // return a no-op mouse interaction to indicate that we handled the press
+
+    // figure out where the click landed
+    const label = this.label, dp = pos[0] - label.x - label.xoffset.current
+    const doff = label.span.current.computeOffset(dp)
+
+    // update selection depending on number of clicks
+    let sel :Selector
+    switch (event.detail) {
+    case 1: sel = charSelector(label.text.current, doff, this.textState) ; break
+    case 2: sel = wordSelector(label.text.current, doff, this.textState) ; break
+    default:
+    case 3: sel = allSelector(label.text.current, doff, this.textState) ; break
+    }
+
+    // as the mouse moves, move the cursor and update the selection
     return {
-      move: (event, pos) => {},
+      move: (event, pos) => {
+        const mp = pos[0] - label.x - label.xoffset.current
+        sel(label.span.current.computeOffset(mp), this.textState)
+      },
       release: () => {},
       cancel: () => {}
     }
@@ -245,12 +407,12 @@ export class Text extends Control {
       if (binding) {
         const action = actions[binding]
         if (action) {
-          action(this.text, this.coffset, typed)
+          action(this.textState, typed)
         } else {
           console.warn(`Invalid binding for ${event.key} (mods: ${mask}): '${action}'`)
         }
       } else if (isPrintable) {
-        actions.insert(this.text, this.coffset, typed)
+        actions.insert(this.textState, typed)
       }
     }
     // let the browser know we handled this event
