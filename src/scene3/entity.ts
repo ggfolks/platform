34f4750aa88ea34507fd7fe02b0d84a013bc948d
Andrzej Kapolka <drzej.k@gmail.com>
2019-07-28 23:14:28 -0700

    Entify camera, lights; have SceneSystem manage the scene and cameras.

--- src/scene3/entity.ts
+++ src/scene3/entity.ts
@@ -1,14 +1,20 @@
 import {
+  AmbientLight,
   BoxBufferGeometry,
+  Camera,
   Color,
-  Group,
+  DirectionalLight,
   Mesh,
   MeshBasicMaterial,
   MeshStandardMaterial,
   MeshToonMaterial,
   Object3D,
+  PerspectiveCamera,
   RGBAFormat,
+  Scene,
   SphereBufferGeometry,
+  Vector2,
+  WebGLRenderer,
 } from "three"
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader"
 
@@ -27,6 +33,27 @@ export interface Object3DConfig {
 export interface GLTFConfig extends Object3DConfig {
   type :"gltf"
   url :string
+}
+
+/** Configures a perspective camera. */
+export interface PerspectiveCameraConfig extends Object3DConfig {
+  type :"perspectiveCamera"
+}
+
+/** Base config for lights. */
+export interface LightConfig extends Object3DConfig {
+  color?: number
+  intensity? :number
+}
+
+/** Configures a directional light. */
+export interface DirectionalLightConfig extends LightConfig {
+  type :"directionalLight"
+}
+
+/** Configures an ambient light. */
+export interface AmbientLightConfig extends LightConfig {
+  type :"ambientLight"
 }
 
 /** Configures the geometry and (optionally) material of a mesh. */
@@ -69,12 +96,16 @@ export interface ToonMaterialConfig extends MaterialConfig {
   color :Color | string | undefined
 }
 
+const rendererSize = new Vector2()
+
 /** Manages a group of scene nodes based on [[TransformComponent]] for 3D transform and a scene
  * object component. Users of this system must call [[SceneSystem.update]] on every frame. */
 export class SceneSystem extends System {
 
-  /** The group that holds all of our objects. */
-  readonly group :Group = new Group()
+  /** The scene that holds all of our objects. */
+  readonly scene :Scene = new Scene()
+
+  private _cameras :Set<Camera> = new Set()
 
   constructor (domain :Domain,
                readonly trans :TransformComponent,
@@ -82,6 +113,21 @@ export class SceneSystem extends System {
     super(domain, Matcher.hasAllC(trans.id, obj.id))
   }
 
+  /** Renders the scene. */
+  render (renderer :WebGLRenderer) {
+    this.update()
+    renderer.getSize(rendererSize)
+    const aspect = rendererSize.x / rendererSize.y
+    for (const camera of this._cameras) {
+      if (camera instanceof PerspectiveCamera && camera.aspect !== aspect) {
+        camera.aspect = aspect
+        camera.updateProjectionMatrix()
+      }
+      renderer.render(this.scene, camera)
+    }
+  }
+
+  /** Updates the transforms of the scene.  Called automatically by [[render]]. */
   update () {
     this.onEntities(id => {
       const obj = this.obj.read(id)
@@ -94,16 +140,21 @@ export class SceneSystem extends System {
   protected added (id :ID, config :EntityConfig) {
     super.added(id, config)
     createObject3D(config.components[this.obj.id]).onValue(obj => {
-      // if this is the initial, default Object3D, it won't actually be in the group;
+      // if this is the initial, default Object3D, it won't actually be in the scene;
       // otherwise, we're replacing the model with another
-      this.group.remove(this.obj.read(id))
+      const oldObj = this.obj.read(id)
+      this.scene.remove(oldObj)
+      if (oldObj instanceof Camera) this._cameras.delete(oldObj)
       this.obj.update(id, obj)
-      this.group.add(obj)
+      this.scene.add(obj)
+      if (obj instanceof Camera) this._cameras.add(obj)
     })
   }
 
   protected deleted (id :ID) {
-    this.group.remove(this.obj.read(id))
+    const obj = this.obj.read(id)
+    this.scene.remove(obj)
+    if (obj instanceof Camera) this._cameras.delete(obj)
     super.deleted(id)
   }
 }
@@ -113,6 +164,17 @@ function createObject3D (objectConfig: Object3DConfig) :Subject<Object3D> {
     case "gltf":
       const gltfConfig = objectConfig as GLTFConfig
       return loadGLTF(gltfConfig.url).map(original => original.clone())
+
+    case "perspectiveCamera":
+      return Value.constant(new PerspectiveCamera())
+
+    case "directionalLight":
+      const dlConfig = objectConfig as DirectionalLightConfig
+      return Value.constant(new DirectionalLight(dlConfig.color, dlConfig.intensity))
+
+    case "ambientLight":
+      const alConfig = objectConfig as AmbientLightConfig
+      return Value.constant(new AmbientLight(alConfig.color, alConfig.intensity))
 
     case "mesh":
       const meshConfig = objectConfig as MeshConfig
