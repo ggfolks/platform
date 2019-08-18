40ae6fc49aacac7201dfbb5d7d18700ce74dfd98
Andrzej Kapolka <drzej.k@gmail.com>
2019-08-18 16:54:38 -0700

    Don't subclass Model; merge model data.

--- src/ui/node.ts
+++ src/ui/node.ts
@@ -7,7 +7,7 @@ import {Graph} from "../graph/graph"
 import {inputEdge} from "../graph/meta"
 import {Node, NodeConfig, NodeContext, NodeTypeRegistry} from "../graph/node"
 import {Host, Root, RootConfig} from "./element"
-import {Model, ModelData, ModelKey, ModelValue, Spec, mapProvider} from "./model"
+import {Model, ModelData, ModelKey, mapProvider} from "./model"
 import {Theme, UI} from "./ui"
 import {ImageResolver, StyleDefs} from "./style"
 
@@ -42,61 +42,52 @@ class UINode extends Node {
         while (graph.ctx.subgraph) graph = graph.ctx.subgraph.graph
         const ctx = this.graph.ctx as UINodeContext
         let root :Root | undefined
-        class NodeModel extends Model {
-          resolve<V extends ModelValue> (spec :Spec<V>) :V {
-            // special handling for remove action and node data
-            if (spec === "remove") {
-              const action = () => {
-                if (root) {
-                  ctx.host.removeRoot(root)
-                  root.dispose()
-                }
-              }
-              return action as V
-            } else if (spec === "nodeKeys") {
-              return graph.nodes.keysSource() as any
-            } else if (spec === "nodeData") {
-              return mapProvider(graph.nodes, value => {
-                const type = value.current.config.type
-                return {
-                  id: Value.constant(value.current.id),
-                  type: Value.constant(type),
-                  propertyKeys: Value.constant(Object.keys(value.current.propertiesMeta)),
-                  inputKeys: Value.constant(Object.keys(value.current.inputsMeta)),
-                  outputKeys: Value.constant(Object.keys(value.current.outputsMeta)),
-                  property: {
-                    resolve: (key :ModelKey) => {
-                      const propertiesMeta = value.current.propertiesMeta[key]
-                      const configValue = value.current.config[key]
-                      return new Model({
-                        name: Value.constant(key),
-                        constraints: Value.constant(propertiesMeta.constraints),
-                        value: Value.constant(
-                          configValue === undefined ? propertiesMeta.defaultValue : configValue,
-                        ),
-                      })
-                    },
-                  },
-                  input: {
-                    resolve: (key :ModelKey) => new Model({
-                      name: Value.constant(key),
-                      multiple: Value.constant(value.current.inputsMeta[key].multiple),
-                      value: Value.constant(value.current.config[key]),
-                    }),
-                  },
-                  output: {
-                    resolve: (key :ModelKey) => new Model({
-                      name: Value.constant(key),
-                      isDefault: Value.constant(value.current.outputsMeta[key].isDefault),
-                    }),
-                  },
-                }
-              }) as any
+        const ui = new UI(ctx.theme, ctx.styles, ctx.image, new Model({
+          ...this.config.model,
+          remove: () => {
+            if (root) {
+              ctx.host.removeRoot(root)
+              root.dispose()
             }
-            return super.resolve(spec)
-          }
-        }
-        const ui = new UI(ctx.theme, ctx.styles, ctx.image, new NodeModel(this.config.model || {}))
+          },
+          nodeKeys: graph.nodes.keysSource(),
+          nodeData: mapProvider(graph.nodes, value => {
+            const type = value.current.config.type
+            return {
+              id: Value.constant(value.current.id),
+              type: Value.constant(type),
+              propertyKeys: Value.constant(Object.keys(value.current.propertiesMeta)),
+              inputKeys: Value.constant(Object.keys(value.current.inputsMeta)),
+              outputKeys: Value.constant(Object.keys(value.current.outputsMeta)),
+              property: {
+                resolve: (key :ModelKey) => {
+                  const propertiesMeta = value.current.propertiesMeta[key]
+                  const configValue = value.current.config[key]
+                  return new Model({
+                    name: Value.constant(key),
+                    constraints: Value.constant(propertiesMeta.constraints),
+                    value: Value.constant(
+                      configValue === undefined ? propertiesMeta.defaultValue : configValue,
+                    ),
+                  })
+                },
+              },
+              input: {
+                resolve: (key :ModelKey) => new Model({
+                  name: Value.constant(key),
+                  multiple: Value.constant(value.current.inputsMeta[key].multiple),
+                  value: Value.constant(value.current.config[key]),
+                }),
+              },
+              output: {
+                resolve: (key :ModelKey) => new Model({
+                  name: Value.constant(key),
+                  isDefault: Value.constant(value.current.outputsMeta[key].isDefault),
+                }),
+              },
+            }
+          }),
+        }))
         root = ui.createRoot(this.config.root)
         if (this.config.size) {
           root.pack(this.config.size[0], this.config.size[1])
