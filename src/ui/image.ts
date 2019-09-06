18fe2aabdb81749528c9f0b6c654e1589d139f0a
Ray J. Greenwell <ray@bogocorp.com>
2019-09-06 14:10:37 -0700

    Allow using only 1 of width or height and scaling the other.

--- src/ui/image.ts
+++ src/ui/image.ts
@@ -30,20 +30,30 @@ export class Image extends Element {
   protected computePreferredSize (hintX :number, hintY :number, into :dim2) {
     const image = this.image.current
     if (image instanceof HTMLImageElement) {
-      dim2.set(into, this.config.width || image.width, this.config.height || image.height)
+      dim2.set(into, this.getWidth(image), this.getHeight(image))
     }
   }
 
   protected relayout () {} // nothing needed
 
   protected rerender (canvas :CanvasRenderingContext2D, region :rect) {
-    const image = this.image.current, {config, x, y} = this
+    const image = this.image.current
     if (image instanceof HTMLImageElement) {
-      if (config.width && config.height) {
-        canvas.drawImage(image, x, y, config.width, config.height)
-      } else {
-        canvas.drawImage(image, x, y)
-      }
+      canvas.drawImage(image, this.x, this.y, this.getWidth(image), this.getHeight(image))
     }
+  }
+
+  protected getWidth (img :HTMLImageElement) :number {
+    const config = this.config
+    if (config.width) return config.width
+    else if (config.height) return (config.height / img.height) * img.width
+    else return img.width
+  }
+
+  protected getHeight (img :HTMLImageElement) :number {
+    const config = this.config
+    if (config.height) return config.height
+    else if (config.width) return (config.width / img.width) * img.height
+    else return img.height
   }
 }
