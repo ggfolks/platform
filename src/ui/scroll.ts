e498b9fe6ab08da46625789aef20bddb4da721af
Michael Bayne <mdb@samskivert.com>
2019-09-24 10:24:03 -0700

    Factor shared bits into TransformedContainer.

--- src/ui/scroll.ts
+++ src/ui/scroll.ts
@@ -9,17 +9,11 @@ export interface PannerConfig extends ControlConfig {
 const transformedPos = vec2.create()
 const transformedRegion = rect.create()
 
-/** Provides a pannable, zoomable window onto its contents. */
-export class Panner extends Control {
-  private readonly _offset = Mutable.local(vec2.create())
-  private readonly _scale = Mutable.local(1)
-  private _laidOut = false
+/** Base class for containers that transform their child. */
+abstract class TransformedContainer extends Control {
 
-  constructor (ctx :ElementContext, parent :Element, readonly config :PannerConfig) {
-    super(ctx, parent, config)
-    this.invalidateOnChange(this._offset)
-    this.invalidateOnChange(this._scale)
-  }
+  get offset () :vec2 { return vec2zero }
+  get scale () :number { return 1 }
 
   applyToContaining (canvas :CanvasRenderingContext2D, pos :vec2, op :(element :Element) => void) {
     if (rect.contains(this.bounds, pos) && this.visible.current) op(this)
@@ -28,6 +22,19 @@ export class Panner extends Control {
   applyToIntersecting (region :rect, op :(element :Element) => void) {
     if (rect.intersects(this.bounds, region) && this.visible.current) op(this)
     this.contents.applyToIntersecting(this._transformRegion(region), op)
+  }
+
+  dirty (region :rect = this.expandBounds(this._bounds), fromChild :boolean = false) {
+    if (!fromChild) {
+      super.dirty(region, false)
+      return
+    }
+    const {x, y, offset, scale} = this
+    transformedRegion[0] = Math.floor(x + scale * (region[0] - x) - offset[0])
+    transformedRegion[1] = Math.floor(y + scale * (region[1] - y) - offset[1])
+    transformedRegion[2] = Math.ceil(scale * region[2])
+    transformedRegion[3] = Math.ceil(scale * region[3])
+    super.dirty(transformedRegion, true)
   }
 
   handlePointerDown (event :MouseEvent|TouchEvent, pos :vec2) :PointerInteraction|undefined {
@@ -40,21 +47,74 @@ export class Panner extends Control {
     }
     if (event instanceof MouseEvent && event.button !== 0) return undefined
     this.root.focus.update(undefined)
-    const basePos = vec2.clone(pos)
-    const baseOffset = this._offset.current
-    const cancel = () => this.clearCursor(this)
-    return {
-      move: (event, pos) => {
-        this.setCursor(this, "all-scroll")
-        this._updateOffset(
-          baseOffset[0] + (basePos[0] - pos[0]),
-          baseOffset[1] + (basePos[1] - pos[1]),
-        )
-      },
-      release: cancel,
-      cancel,
-    }
+    return this.startScroll(event, pos)
   }
+
+  handleDoubleClick (event :MouseEvent, pos :vec2) {
+    const transformedPos = this._transformPos(pos)
+    return this.contents.maybeHandleDoubleClick(event, transformedPos)
+  }
+
+  protected get computeState () :string {
+    return this.enabled.current ? "normal" : "disabled"
+  }
+
+  protected rerender (canvas :CanvasRenderingContext2D, region :rect) {
+    const {x, y, offset, scale} = this
+    canvas.save()
+    canvas.beginPath()
+    canvas.rect(x, y, this.width, this.height)
+    canvas.clip()
+    canvas.translate(x - x*scale - offset[0], y - y*scale - offset[1])
+    canvas.scale(scale, scale)
+    this.contents.render(canvas, this._transformRegion(region))
+    canvas.restore()
+  }
+
+  protected relayout () {
+    const size = this.contents.preferredSize(this.width, this.height)
+    this.contents.setBounds(rect.fromValues(this.x, this.y, size[0], size[1]))
+  }
+
+  protected computePreferredSize (hintX :number, hintY :number, into :dim2) {
+    dim2.set(into, hintX, hintY)
+  }
+
+  protected startScroll (event :MouseEvent|TouchEvent, pos :vec2) :PointerInteraction|undefined {
+    return undefined
+  }
+
+  /** Transforms the supplied position into the space of the contents. */
+  protected _transformPos (pos :vec2) {
+    const {x, y, offset, scale} = this
+    const tx = (pos[0] - x + offset[0]) / scale + x
+    const ty = (pos[1] - y + offset[1]) / scale + y
+    return vec2.set(transformedPos, tx, ty)
+  }
+
+  /** Transforms the supplied region into the space of the contents. */
+  protected _transformRegion (region :rect) {
+    const {x, y, offset, scale} = this
+    const tx = (region[0] - x + offset[0]) / scale + x
+    const ty = (region[1] - y + offset[1]) / scale + y
+    return rect.set(transformedRegion, tx, ty, region[2] / scale, region[3] / scale)
+  }
+}
+
+/** Provides a pannable, zoomable window onto its contents. */
+export class Panner extends TransformedContainer {
+  private readonly _offset = Mutable.local(vec2.create())
+  private readonly _scale = Mutable.local(1)
+  private _laidOut = false
+
+  constructor (ctx :ElementContext, parent :Element, readonly config :PannerConfig) {
+    super(ctx, parent, config)
+    this.invalidateOnChange(this._offset)
+    this.invalidateOnChange(this._scale)
+  }
+
+  get offset () { return this._offset.current }
+  get scale () { return this._scale.current }
 
   handleWheel (event :WheelEvent, pos :vec2) {
     const transformedPos = this._transformPos(pos)
@@ -63,11 +123,6 @@ export class Panner extends Control {
       this.zoom(event.deltaY > 0 ? -1 : 1)
     }
     return true
-  }
-
-  handleDoubleClick (event :MouseEvent, pos :vec2) {
-    const transformedPos = this._transformPos(pos)
-    return this.contents.maybeHandleDoubleClick(event, transformedPos)
   }
 
   /** Zooms in or out by the specified delta. */
@@ -84,6 +139,23 @@ export class Panner extends Control {
   zoomToFit () {
     const size = this.contents.preferredSize(this.width, this.height)
     this._updateScale(Math.min(1, this.width / size[0], this.height / size[1]))
+  }
+
+  protected startScroll (event :MouseEvent|TouchEvent, pos :vec2) :PointerInteraction|undefined {
+    const basePos = vec2.clone(pos)
+    const baseOffset = this._offset.current
+    const cancel = () => this.clearCursor(this)
+    return {
+      move: (event, pos) => {
+        this.setCursor(this, "all-scroll")
+        this._updateOffset(
+          baseOffset[0] + (basePos[0] - pos[0]),
+          baseOffset[1] + (basePos[1] - pos[1]),
+        )
+      },
+      release: cancel,
+      cancel,
+    }
   }
 
   private _updateScale (scale :number) {
@@ -105,74 +177,11 @@ export class Panner extends Control {
     ))
   }
 
-  dirty (region :rect = this.expandBounds(this._bounds), fromChild :boolean = false) {
-    if (!fromChild) {
-      super.dirty(region, false)
-      return
-    }
-    // can be called before properties are initialized
-    const offset = this._offset ? this._offset.current : vec2zero
-    const scale = this._scale ? this._scale.current : 1
-    transformedRegion[0] = Math.floor(this.x + scale * (region[0] - this.x) - offset[0])
-    transformedRegion[1] = Math.floor(this.y + scale * (region[1] - this.y) - offset[1])
-    transformedRegion[2] = Math.ceil(scale * region[2])
-    transformedRegion[3] = Math.ceil(scale * region[3])
-    super.dirty(transformedRegion, true)
-  }
-
-  protected computePreferredSize (hintX :number, hintY :number, into :dim2) {
-    dim2.set(into, hintX, hintY)
-  }
-
   protected relayout () {
-    const size = this.contents.preferredSize(this.width, this.height)
-    this.contents.setBounds(rect.fromValues(this.x, this.y, size[0], size[1]))
-
+    super.relayout()
     // scale to fit on first layout if larger than viewport
     if (this._laidOut) return
     this._laidOut = true
     this.zoomToFit()
-  }
-
-  protected get computeState () :string {
-    return this.enabled.current ? "normal" : "disabled"
-  }
-
-  protected rerender (canvas :CanvasRenderingContext2D, region :rect) {
-    canvas.save()
-    canvas.beginPath()
-    canvas.rect(this.x, this.y, this.width, this.height)
-    canvas.clip()
-    const offset = this._offset.current
-    const scale = this._scale.current
-    canvas.translate(
-      this.x - this.x * scale - offset[0],
-      this.y - this.y * scale - offset[1],
-    )
-    canvas.scale(scale, scale)
-    this.contents.render(canvas, this._transformRegion(region))
-    canvas.restore()
-  }
-
-  /** Transforms the supplied position into the space of the contents. */
-  private _transformPos (pos :vec2) {
-    return vec2.set(
-      transformedPos,
-      (pos[0] - this.x + this._offset.current[0]) / this._scale.current + this.x,
-      (pos[1] - this.y + this._offset.current[1]) / this._scale.current + this.y,
-    )
-  }
-
-  /** Transforms the supplied region into the space of the contents. */
-  private _transformRegion (region :rect) {
-    const offset = this._offset.current
-    const scale = this._scale.current
-    return rect.set(
-      transformedRegion,
-      (region[0] - this.x + offset[0]) / scale + this.x,
-      (region[1] - this.y + offset[1]) / scale + this.y,
-      region[2] / scale,
-      region[3] / scale,
-    )
   }
 }
